23d5802156c237d417eec68aaae94dfed07f3430
SamOluwadamilare300 <damilarea71@gmail.com>
2024-08-19 02:21:06 -0700

    Single React App

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./App.css";
+import Header from './Components/header/header';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <main className='main' >
+        <Home/>
+        <About/>
+      </main>
+    </>
+  )
+}
+
+export default App;
+
